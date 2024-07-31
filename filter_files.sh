4fbcf4951b31bee0d01c2825882bd41efe24e145
sonephet <sonephet.dev@gmail.com>
2024-07-31 17:49:11 +0700

    update deploy 1

--- filter_files.sh
+++ filter_files.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 source_dir="/root/servers/actions-runner/JUTSUN-API/jutsun-node/jutsun-node"
-dest_dir="/root"  # Create a temporary directory outside the project
-exclude_dirs=("uploads")
 
+# Option 1: Using /tmp/filtered_files
+dest_dir="/tmp/filtered_files"
 mkdir -p "$dest_dir"
 
+# Option 2: Creating a unique temporary directory
+# dest_dir=$(mktemp -d "/tmp/filtered_files_XXXXXX")
+
+exclude_dirs=("uploads")
+
 find "$source_dir" -type f \( ! -path "*${exclude_dirs[@]}*" \) -exec cp -r {} "$dest_dir" \;
+
+# Option 2: Clean up the temporary directory (if using mktemp)
+# rm -rf "$dest_dir"  # Uncomment this line if using Option 2
