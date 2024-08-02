3162359660466a8bd266cb0d4a244d086a4f9769
sonephet <sonephet.dev@gmail.com>
2024-08-02 13:29:40 +0700

    update api repor

--- src/apis/receive/service.ts
+++ src/apis/receive/service.ts
@@ -1,21 +1,46 @@
-import logger from "../../configs/winston"
-import prismaClient from "../../prisma"
+import logger from '../../configs/winston'
+import prismaClient from '../../prisma'
 
-export const readReceiveService = async (data: { companyId: number, dateStart: string, dateEnd: string }) => {
-    console.log("", data)
+export const readReceiveService = async (data: { companyId: number; dateStart: string; dateEnd: string }) => {
+    console.log('readReceiveService == ', data)
     const { companyId, dateStart, dateEnd } = data
     try {
-        const receive: any[] = await prismaClient.$queryRaw` SELECT inv.paymentMethod, SUM(inv.amount) as totalAmount, inv.currency, CONCAT(u.fullName, ' ', u.lastName) createdBy
-FROM invoice inv
-LEFT JOIN contracts ct on inv.contractId = ct.contractId
-LEFT JOIN company cp on cp.companyId = ct.companyId
-LEFT JOIN users u on u.userId = inv.createdBy
-WHERE cp.companyId = ${companyId}
-AND DATE(paidDate) BETWEEN ${dateStart} AND ${dateEnd}
-GROUP BY paymentMethod, currency, createdBy
-ORDER BY createdBy`
-        return receive
+        const receive: any[] = await prismaClient.$queryRaw`
+            SELECT inv.paymentMethod, SUM(inv.amount) as totalAmount, inv.currency, CONCAT(u.fullName, ' ', u.lastName) createdBy
+            FROM invoice inv
+            LEFT JOIN contracts ct on inv.contractId = ct.contractId
+            LEFT JOIN company cp on cp.companyId = ct.companyId
+            LEFT JOIN users u on u.userId = inv.createdBy
+            WHERE cp.companyId = ${companyId}
+            AND DATE(paidDate) BETWEEN ${dateStart} AND ${dateEnd}
+            GROUP BY paymentMethod, currency, createdBy
+            ORDER BY createdBy
+            `
 
+        return receive
+    } catch (err) {
+        logger.error(err)
+        console.log(err)
+        return []
+    } finally {
+        await prismaClient.$disconnect()
+    }
+}
+
+export const summaryReceiveService = async (data: { companyId: number; dateStart: string; dateEnd: string }) => {
+    const { companyId, dateStart, dateEnd } = data
+    try {
+        const receive: any[] = await prismaClient.$queryRaw`
+            SELECT inv.paymentMethod, SUM(inv.amount) as totalAmount, inv.currency
+            FROM invoice inv
+            LEFT JOIN contracts ct on inv.contractId = ct.contractId
+            LEFT JOIN company cp on cp.companyId = ct.companyId
+            WHERE cp.companyId = ${companyId}
+            AND DATE(inv.paidDate) BETWEEN ${dateStart} AND ${dateEnd}
+            GROUP BY inv.paymentMethod, inv.currency
+            `
+
+        return receive
     } catch (err) {
         logger.error(err)
         console.log(err)
