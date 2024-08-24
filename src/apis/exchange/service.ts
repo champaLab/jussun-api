bfeb93d861e5450f06738a8d6bf48aec2aa181e5
pepsiDev <sevongsainvonnthep@gmail.com>
2024-08-24 12:06:08 +0700

    news

--- src/apis/exchange/service.ts
+++ src/apis/exchange/service.ts
@@ -8,6 +8,7 @@ export const readExchangeService = async (data: { companyId: number; dateStart: 
     const skip = (data.page - 1) * env.ROW_PER_PAGE
     const take = env.ROW_PER_PAGE
     const { companyId, dateStart, dateEnd } = data
+    console.log("@@@",data)
     try {
         if (dateStart && dateEnd) {
             const totalPage: any[] = await prismaClient.$queryRaw`
